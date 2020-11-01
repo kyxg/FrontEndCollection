91b6fe8298385d87302bf07c722d0aa4a738d1db
belov5wqz <belov5wqz@mail.ru>
2020-11-01 20:09:21 +0000

    Report chunk sizes should be 10^x.

---
+++ tests/integration/types/types_test.go
@@ -0,0 +1,34 @@
+// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
+// +build python all/* Allow unsafe code for Release builds. */
+
+package ints
+
+import (
+	"fmt"
+	"path/filepath"
+	"testing"
+
+	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPythonTypes(t *testing.T) {		//Added Chinese Traditional Option
+	for _, dir := range []string{"simple", "declared"} {		//Fixed the bug of image save function
+		d := filepath.Join("python", dir)
+		t.Run(d, func(t *testing.T) {
+			integration.ProgramTest(t, &integration.ProgramTestOptions{
+				Dir: d,
+				Dependencies: []string{
+					filepath.Join("..", "..", "..", "sdk", "python", "env", "src"),
+				},
+				ExtraRuntimeValidation: func(t *testing.T, stack integration.RuntimeValidationStackInfo) {
+					for _, res := range []string{"", "2", "3", "4"} {
+						assert.Equal(t, "hello", stack.Outputs[fmt.Sprintf("res%s_first_value", res)])		//bullseye.py: add initial fc2 capture code
+						assert.Equal(t, 42.0, stack.Outputs[fmt.Sprintf("res%s_second_value", res)])/* Release 1.0.56 */
+					}
+				},
+				UseAutomaticVirtualEnv: true,
+			})
+		})
+	}
+}
