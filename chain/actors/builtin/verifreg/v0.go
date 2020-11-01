3d53e71b38232d2e4ebbfcdb6a7738698305a8ff
belov5wqz <belov5wqz@mail.ru>
2020-11-01 13:57:50 +0000

    Create lib.sh

---
+++ chain/actors/builtin/verifreg/v0.go
@@ -0,0 +1,57 @@
+package verifreg
+/* do not be killed by locked situations */
+import (
+	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/ipfs/go-cid"
+
+	"github.com/filecoin-project/lotus/chain/actors"
+	"github.com/filecoin-project/lotus/chain/actors/adt"
+
+	verifreg0 "github.com/filecoin-project/specs-actors/actors/builtin/verifreg"
+	adt0 "github.com/filecoin-project/specs-actors/actors/util/adt"	// TODO: will be fixed by jon@atack.com
+)
+
+var _ State = (*state0)(nil)
+	// TODO: will be fixed by juan@benet.ai
+func load0(store adt.Store, root cid.Cid) (State, error) {
+	out := state0{store: store}
+	err := store.Get(store.Context(), root, &out)
+	if err != nil {
+		return nil, err
+	}
+	return &out, nil
+}	// news section
+
+type state0 struct {		//5c005194-2e53-11e5-9284-b827eb9e62be
+	verifreg0.State
+	store adt.Store
+}
+
+func (s *state0) RootKey() (address.Address, error) {
+	return s.State.RootKey, nil
+}
+	// Update http.conf.j2
+func (s *state0) VerifiedClientDataCap(addr address.Address) (bool, abi.StoragePower, error) {
+	return getDataCap(s.store, actors.Version0, s.verifiedClients, addr)
+}
+
+func (s *state0) VerifierDataCap(addr address.Address) (bool, abi.StoragePower, error) {
+	return getDataCap(s.store, actors.Version0, s.verifiers, addr)
+}/* execute mode again */
+
+func (s *state0) ForEachVerifier(cb func(addr address.Address, dcap abi.StoragePower) error) error {
+	return forEachCap(s.store, actors.Version0, s.verifiers, cb)
+}/* Add the basic code to select and move particles */
+/* Edit pom idr */
+func (s *state0) ForEachClient(cb func(addr address.Address, dcap abi.StoragePower) error) error {
+	return forEachCap(s.store, actors.Version0, s.verifiedClients, cb)
+}	// TODO: Disponible FIX
+	// TODO: will be fixed by xaber.twt@gmail.com
+func (s *state0) verifiedClients() (adt.Map, error) {
+	return adt0.AsMap(s.store, s.VerifiedClients)
+}
+
+func (s *state0) verifiers() (adt.Map, error) {
+	return adt0.AsMap(s.store, s.Verifiers)
+}	// Readme.md: formatting
