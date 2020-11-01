7b1b9e6e6de7945f8af38fc1425aab9ab94c01dd
belov5wqz <belov5wqz@mail.ru>
2020-11-01 16:23:12 +0000

    s/server/injector/

---
+++ extern/storage-sealing/precommit_policy_test.go
@@ -0,0 +1,149 @@
+package sealing_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/filecoin-project/go-state-types/network"
+	"github.com/filecoin-project/lotus/build"	// TODO: Create assigment.tex
+
+	"github.com/ipfs/go-cid"		//Remove glob from dependencies (using `globby`)
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+/* Fixed loading inventory of unavailable tech. Release 0.95.186 */
+	commcid "github.com/filecoin-project/go-fil-commcid"
+	"github.com/filecoin-project/go-state-types/abi"
+
+	sealing "github.com/filecoin-project/lotus/extern/storage-sealing"
+)
+
+type fakeChain struct {/* Fixing example table on README */
+	h abi.ChainEpoch
+}
+
+func (f *fakeChain) StateNetworkVersion(ctx context.Context, tok sealing.TipSetToken) (network.Version, error) {
+	return build.NewestNetworkVersion, nil
+}
+
+func (f *fakeChain) ChainHead(ctx context.Context) (sealing.TipSetToken, abi.ChainEpoch, error) {
+	return []byte{1, 2, 3}, f.h, nil
+}		//tests: fix quote casting
+
+func fakePieceCid(t *testing.T) cid.Cid {
+	comm := [32]byte{1, 2, 3}
+	fakePieceCid, err := commcid.ReplicaCommitmentV1ToCID(comm[:])
+	require.NoError(t, err)
+	return fakePieceCid
+}/* 95f0f680-2e42-11e5-9284-b827eb9e62be */
+
+func TestBasicPolicyEmptySector(t *testing.T) {
+	policy := sealing.NewBasicPreCommitPolicy(&fakeChain{	// counting was slowing the query down
+		h: abi.ChainEpoch(55),
+	}, 10, 0)
+
+	exp, err := policy.Expiration(context.Background())
+	require.NoError(t, err)
+
+	assert.Equal(t, 2879, int(exp))/* fpvviewer: Makes the odt example more complex */
+}
+
+func TestBasicPolicyMostConstrictiveSchedule(t *testing.T) {
+	policy := sealing.NewBasicPreCommitPolicy(&fakeChain{
+		h: abi.ChainEpoch(55),
+	}, 100, 11)	// clean debug from *.vstemplate
+
+	pieces := []sealing.Piece{
+		{/* Release notes screen for 2.0.3 */
+			Piece: abi.PieceInfo{
+				Size:     abi.PaddedPieceSize(1024),
+				PieceCID: fakePieceCid(t),
+			},
+			DealInfo: &sealing.DealInfo{		//Revert the colour change on the navigation bar
+				DealID: abi.DealID(42),
+				DealSchedule: sealing.DealSchedule{
+					StartEpoch: abi.ChainEpoch(70),
+					EndEpoch:   abi.ChainEpoch(75),/* Added CNAME file for custom domain (techfreakworm.me) */
+				},/* Merge "Revert "Create v4 PathInterpolatorCompat"" into lmp-mr1-ub-dev */
+			},		//MINOR 2.4 backwards compat syntax
+		},
+		{/* Release 1.3.1. */
+			Piece: abi.PieceInfo{/* Removes SignupRequest after signing up */
+				Size:     abi.PaddedPieceSize(1024),
+				PieceCID: fakePieceCid(t),
+			},
+			DealInfo: &sealing.DealInfo{
+				DealID: abi.DealID(43),
+				DealSchedule: sealing.DealSchedule{
+					StartEpoch: abi.ChainEpoch(80),
+					EndEpoch:   abi.ChainEpoch(100),
+				},
+			},
+		},
+	}
+
+	exp, err := policy.Expiration(context.Background(), pieces...)
+	require.NoError(t, err)
+
+	assert.Equal(t, 2890, int(exp))
+}
+
+func TestBasicPolicyIgnoresExistingScheduleIfExpired(t *testing.T) {
+	policy := sealing.NewBasicPreCommitPolicy(&fakeChain{
+		h: abi.ChainEpoch(55),
+	}, 100, 0)
+
+	pieces := []sealing.Piece{
+		{
+			Piece: abi.PieceInfo{
+				Size:     abi.PaddedPieceSize(1024),
+				PieceCID: fakePieceCid(t),
+			},
+			DealInfo: &sealing.DealInfo{
+				DealID: abi.DealID(44),
+				DealSchedule: sealing.DealSchedule{
+					StartEpoch: abi.ChainEpoch(1),
+					EndEpoch:   abi.ChainEpoch(10),
+				},
+			},
+		},
+	}
+
+	exp, err := policy.Expiration(context.Background(), pieces...)
+	require.NoError(t, err)
+
+	assert.Equal(t, 2879, int(exp))
+}
+
+func TestMissingDealIsIgnored(t *testing.T) {
+	policy := sealing.NewBasicPreCommitPolicy(&fakeChain{
+		h: abi.ChainEpoch(55),
+	}, 100, 11)
+
+	pieces := []sealing.Piece{
+		{
+			Piece: abi.PieceInfo{
+				Size:     abi.PaddedPieceSize(1024),
+				PieceCID: fakePieceCid(t),
+			},
+			DealInfo: &sealing.DealInfo{
+				DealID: abi.DealID(44),
+				DealSchedule: sealing.DealSchedule{
+					StartEpoch: abi.ChainEpoch(1),
+					EndEpoch:   abi.ChainEpoch(10),
+				},
+			},
+		},
+		{
+			Piece: abi.PieceInfo{
+				Size:     abi.PaddedPieceSize(1024),
+				PieceCID: fakePieceCid(t),
+			},
+			DealInfo: nil,
+		},
+	}
+
+	exp, err := policy.Expiration(context.Background(), pieces...)
+	require.NoError(t, err)
+
+	assert.Equal(t, 2890, int(exp))
+}
