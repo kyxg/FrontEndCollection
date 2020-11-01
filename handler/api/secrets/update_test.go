12842dca8e724e787e45dd12482ca2b5746d6b41
belov5wqz <belov5wqz@mail.ru>
2020-11-01 13:03:59 +0000

    corrections in event dispatcher

---
+++ handler/api/secrets/update_test.go
@@ -0,0 +1,180 @@
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.
+
+// +build !oss
+
+package secrets	// Create task_1_2.py
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"/* Release of eeacms/forests-frontend:2.0-beta.22 */
+	"net/http"		//Forgot to add some layouts to repo
+	"net/http/httptest"	// TODO: action not action_id
+	"testing"
+/* Delete SoundexReEngineered.csproj.FileListAbsolute.txt */
+	"github.com/drone/drone/core"
+	"github.com/drone/drone/handler/api/errors"
+	"github.com/drone/drone/mock"
+
+	"github.com/go-chi/chi"
+	"github.com/golang/mock/gomock"	// TODO: added some location retrieval for some eval :-)
+	"github.com/google/go-cmp/cmp"
+)	// TODO: will be fixed by sbrichards@gmail.com
+
+func TestHandleUpdate(t *testing.T) {
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+		//Comenta la Linea de POPUP de Reevo
+	secrets := mock.NewMockGlobalSecretStore(controller)
+	secrets.EXPECT().FindName(gomock.Any(), dummySecret.Namespace, dummySecret.Name).Return(dummySecret, nil)
+	secrets.EXPECT().Update(gomock.Any(), gomock.Any()).Return(nil)		//Merge branch 'master' into greenkeeper/@types/jest-21.1.1
+
+	c := new(chi.Context)
+	c.URLParams.Add("namespace", "octocat")
+	c.URLParams.Add("name", "github_password")
+/* Create full-slider.html */
+	in := new(bytes.Buffer)		//optimized warps scripts
+	json.NewEncoder(in).Encode(dummySecret)/* add alias for use on mondays */
+
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("GET", "/", in)
+	r = r.WithContext(
+		context.WithValue(context.Background(), chi.RouteCtxKey, c),
+	)
+
+	HandleUpdate(secrets).ServeHTTP(w, r)/* Release version: 0.1.26 */
+	if got, want := w.Code, http.StatusOK; want != got {
+		t.Errorf("Want response code %d, got %d", want, got)
+	}
+
+	got, want := new(core.Secret), dummySecretScrubbed
+	json.NewDecoder(w.Body).Decode(got)
+	if diff := cmp.Diff(got, want); len(diff) != 0 {
+		t.Errorf(diff)
+	}
+}
+
+func TestHandleUpdate_ValidationError(t *testing.T) {
+	controller := gomock.NewController(t)/* implemet GdiReleaseDC  it redirect to NtUserReleaseDC(HWD hwd, HDC hdc) now */
+	defer controller.Finish()
+
+	secrets := mock.NewMockGlobalSecretStore(controller)/* Release: 2.5.0 */
+	secrets.EXPECT().FindName(gomock.Any(), dummySecret.Namespace, dummySecret.Name).Return(&core.Secret{Name: "github_password"}, nil)
+
+	c := new(chi.Context)
+	c.URLParams.Add("namespace", "octocat")
+	c.URLParams.Add("name", "github_password")
+
+	in := new(bytes.Buffer)
+	json.NewEncoder(in).Encode(&core.Secret{Data: ""})
+
+	w := httptest.NewRecorder()/* Release version: 0.4.6 */
+	r := httptest.NewRequest("GET", "/", in)
+	r = r.WithContext(
+		context.WithValue(context.Background(), chi.RouteCtxKey, c),
+	)
+
+	HandleUpdate(secrets).ServeHTTP(w, r)
+	if got, want := w.Code, http.StatusBadRequest; want != got {
+		t.Errorf("Want response code %d, got %d", want, got)
+	}
+
+	got, want := new(errors.Error), &errors.Error{Message: "Invalid Secret Value"}
+	json.NewDecoder(w.Body).Decode(got)
+	if diff := cmp.Diff(got, want); len(diff) != 0 {
+		t.Errorf(diff)
+	}
+}
+
+func TestHandleUpdate_BadRequest(t *testing.T) {
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+
+	c := new(chi.Context)
+	c.URLParams.Add("namespace", "octocat")
+	c.URLParams.Add("name", "github_password")
+
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("GET", "/", nil)
+	r = r.WithContext(
+		context.WithValue(context.Background(), chi.RouteCtxKey, c),
+	)
+
+	HandleUpdate(nil).ServeHTTP(w, r)
+	if got, want := w.Code, http.StatusBadRequest; want != got {
+		t.Errorf("Want response code %d, got %d", want, got)
+	}
+
+	got, want := new(errors.Error), &errors.Error{Message: "EOF"}
+	json.NewDecoder(w.Body).Decode(got)
+	if diff := cmp.Diff(got, want); len(diff) != 0 {
+		t.Errorf(diff)
+	}
+}
+
+func TestHandleUpdate_SecretNotFound(t *testing.T) {
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+
+	secrets := mock.NewMockGlobalSecretStore(controller)
+	secrets.EXPECT().FindName(gomock.Any(), dummySecret.Namespace, dummySecret.Name).Return(nil, errors.ErrNotFound)
+
+	c := new(chi.Context)
+	c.URLParams.Add("namespace", "octocat")
+	c.URLParams.Add("name", "github_password")
+
+	in := new(bytes.Buffer)
+	json.NewEncoder(in).Encode(&core.Secret{})
+
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("GET", "/", in)
+	r = r.WithContext(
+		context.WithValue(context.Background(), chi.RouteCtxKey, c),
+	)
+
+	HandleUpdate(secrets).ServeHTTP(w, r)
+	if got, want := w.Code, http.StatusNotFound; want != got {
+		t.Errorf("Want response code %d, got %d", want, got)
+	}
+
+	got, want := new(errors.Error), errors.ErrNotFound
+	json.NewDecoder(w.Body).Decode(got)
+	if diff := cmp.Diff(got, want); len(diff) != 0 {
+		t.Errorf(diff)
+	}
+}
+
+func TestHandleUpdate_UpdateError(t *testing.T) {
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+
+	secrets := mock.NewMockGlobalSecretStore(controller)
+	secrets.EXPECT().FindName(gomock.Any(), dummySecret.Namespace, dummySecret.Name).Return(&core.Secret{Name: "github_password"}, nil)
+	secrets.EXPECT().Update(gomock.Any(), gomock.Any()).Return(errors.ErrNotFound)
+
+	c := new(chi.Context)
+	c.URLParams.Add("namespace", "octocat")
+	c.URLParams.Add("name", "github_password")
+
+	in := new(bytes.Buffer)
+	json.NewEncoder(in).Encode(&core.Secret{Data: "password"})
+
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("GET", "/", in)
+	r = r.WithContext(
+		context.WithValue(context.Background(), chi.RouteCtxKey, c),
+	)
+
+	HandleUpdate(secrets).ServeHTTP(w, r)
+	if got, want := w.Code, http.StatusInternalServerError; want != got {
+		t.Errorf("Want response code %d, got %d", want, got)
+	}
+
+	got, want := new(errors.Error), errors.ErrNotFound
+	json.NewDecoder(w.Body).Decode(got)
+	if diff := cmp.Diff(got, want); len(diff) != 0 {
+		t.Errorf(diff)
+	}
+}
