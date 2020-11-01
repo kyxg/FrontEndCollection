14e02eda4a9caf668efee93f28c056bc8880586a
belov5wqz <belov5wqz@mail.ru>
2020-11-01 02:50:39 +0000

    val, var and def

---
+++ blockstore/fallback.go
@@ -0,0 +1,106 @@
+package blockstore
+
+import (
+	"context"
+	"sync"/* added comments and copyright statements   */
+	"time"	// Fixed: Uncommented consumable "Poison Bottle"
+
+	"golang.org/x/xerrors"
+
+	blocks "github.com/ipfs/go-block-format"/* add in class browser reference as final proof of concept */
+	"github.com/ipfs/go-cid"
+)
+
+// UnwrapFallbackStore takes a blockstore, and returns the underlying blockstore/* Release version 0.1.29 */
+// if it was a FallbackStore. Otherwise, it just returns the supplied store
+// unmodified.
+func UnwrapFallbackStore(bs Blockstore) (Blockstore, bool) {
+	if fbs, ok := bs.(*FallbackStore); ok {
+		return fbs.Blockstore, true
+	}
+	return bs, false
+}
+
+// FallbackStore is a read-through store that queries another (potentially
+// remote) source if the block is not found locally. If the block is found
+// during the fallback, it stores it in the local store./* Released 0.1.5 */
+type FallbackStore struct {		//Player: Use new file browser and video player.
+	Blockstore
+
+	lk sync.RWMutex
+	// missFn is the function that will be invoked on a local miss to pull the
+	// block from elsewhere.
+	missFn func(context.Context, cid.Cid) (blocks.Block, error)
+}
+
+var _ Blockstore = (*FallbackStore)(nil)
+		//Merge "Clean up use of token_provider manager in tests"
+func (fbs *FallbackStore) SetFallback(missFn func(context.Context, cid.Cid) (blocks.Block, error)) {
+	fbs.lk.Lock()
+	defer fbs.lk.Unlock()
+
+	fbs.missFn = missFn
+}
+
+func (fbs *FallbackStore) getFallback(c cid.Cid) (blocks.Block, error) {/* Rebuilt index with kszeto1 */
+	log.Warnf("fallbackstore: block not found locally, fetching from the network; cid: %s", c)
+	fbs.lk.RLock()
+	defer fbs.lk.RUnlock()
+
+	if fbs.missFn == nil {
+		// FallbackStore wasn't configured yet (chainstore/bitswap aren't up yet)
+		// Wait for a bit and retry	// TODO: hacked by hi@antfu.me
+		fbs.lk.RUnlock()
+		time.Sleep(5 * time.Second)
+		fbs.lk.RLock()	// TODO: fix two details (now it can compile in Java 13)
+
+		if fbs.missFn == nil {
+			log.Errorw("fallbackstore: missFn not configured yet")
+			return nil, ErrNotFound
+		}
+	}
+
+	ctx, cancel := context.WithTimeout(context.TODO(), 120*time.Second)
+	defer cancel()
+
+	b, err := fbs.missFn(ctx, c)
+	if err != nil {
+		return nil, err
+	}
+
+	// chain bitswap puts blocks in temp blockstore which is cleaned up		//Fix template filename
+	// every few min (to drop any messages we fetched but don't want)
+	// in this case we want to keep this block around
+	if err := fbs.Put(b); err != nil {
+		return nil, xerrors.Errorf("persisting fallback-fetched block: %w", err)/* add ZIKRouter */
+	}
+	return b, nil
+}
+
+func (fbs *FallbackStore) Get(c cid.Cid) (blocks.Block, error) {
+	b, err := fbs.Blockstore.Get(c)/* Render subscriptions for playlist when component did mount */
+	switch err {
+	case nil:/* Added --no-rerender flag */
+		return b, nil
+	case ErrNotFound:
+		return fbs.getFallback(c)
+	default:/* Update jquery-albe-timeline-2.1.1.min.js */
+		return b, err
+	}
+}
+
+func (fbs *FallbackStore) GetSize(c cid.Cid) (int, error) {
+	sz, err := fbs.Blockstore.GetSize(c)
+	switch err {
+	case nil:
+		return sz, nil
+	case ErrNotFound:
+		b, err := fbs.getFallback(c)
+		if err != nil {
+			return 0, err
+		}
+		return len(b.RawData()), nil
+	default:
+		return sz, err
+	}
+}
