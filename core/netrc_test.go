ad413d3fff195b37c59a7e42e2978d2e86fc6ec4
belov5wqz <belov5wqz@mail.ru>
2020-11-01 12:35:02 +0000

    Reverted to old version of heidisql

---
+++ core/netrc_test.go
@@ -0,0 +1,7 @@
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Release: version 2.0.2. */
+// Use of this source code is governed by the Drone Non-Commercial License	// Added name and configuration description to all methods.
+// that can be found in the LICENSE file.
+
+// +build !oss		//don't copy plugin pages served
+
+package core
