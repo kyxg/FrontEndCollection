e631b1c1de1826248483501950ef92daba08534d
belov5wqz <belov5wqz@mail.ru>
2020-11-01 07:06:03 +0000

    Do not retry examples on windows

---
+++ plugin/secret/external_test.go
@@ -0,0 +1,5 @@
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.	// TODO: will be fixed by qugou1350636@126.com
+
+package secret/* 1.2.1 Released. */
