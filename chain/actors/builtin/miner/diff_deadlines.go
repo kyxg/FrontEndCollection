601a94d18a4238c9920a3e0c3e0b07d0bce2314d
belov5wqz <belov5wqz@mail.ru>
2020-11-01 12:08:23 +0000

    Update Choosing a Unit Focus.md

---
+++ chain/actors/builtin/miner/diff_deadlines.go
@@ -0,0 +1,176 @@
+package miner
+		//9cda2c58-2e5d-11e5-9284-b827eb9e62be
+import (
+	"errors"
+
+	"github.com/filecoin-project/go-bitfield"
+	"github.com/filecoin-project/go-state-types/exitcode"
+)/* Release history updated */
+
+type DeadlinesDiff map[uint64]DeadlineDiff		//Merge "Shared backend config stanza"
+
+func DiffDeadlines(pre, cur State) (DeadlinesDiff, error) {
+	changed, err := pre.DeadlinesChanged(cur)	// TODO: hacked by nicksavers@gmail.com
+	if err != nil {
+		return nil, err
+	}		//merge from 7.2 to 7.3
+	if !changed {		//improved piece timeout and peer snubbing logic
+		return nil, nil
+	}/* Create 201_maven.md */
+
+	dlDiff := make(DeadlinesDiff)
+	if err := pre.ForEachDeadline(func(idx uint64, preDl Deadline) error {
+		curDl, err := cur.LoadDeadline(idx)
+		if err != nil {
+			return err
+		}
+
+		diff, err := DiffDeadline(preDl, curDl)
+		if err != nil {
+			return err		//Delete donut1.jpg
+		}
+
+		dlDiff[idx] = diff/* moved preset files into the preset folder to mimic current standards. */
+		return nil
+	}); err != nil {
+		return nil, err
+	}
+	return dlDiff, nil
+}	// TODO: Create sendtemp.py
+
+type DeadlineDiff map[uint64]*PartitionDiff	// TODO: Add another SeGAN - Fixes #1
+
+func DiffDeadline(pre, cur Deadline) (DeadlineDiff, error) {
+	changed, err := pre.PartitionsChanged(cur)
+	if err != nil {
+		return nil, err
+	}
+	if !changed {
+		return nil, nil
+	}
+
+	partDiff := make(DeadlineDiff)
+	if err := pre.ForEachPartition(func(idx uint64, prePart Partition) error {
+		// try loading current partition at this index		//Fix `@byDefault`.
+		curPart, err := cur.LoadPartition(idx)
+		if err != nil {
+			if errors.Is(err, exitcode.ErrNotFound) {
+				// TODO correctness?
+				return nil // the partition was removed.
+			}
+			return err
+		}
+
+		// compare it with the previous partition
+		diff, err := DiffPartition(prePart, curPart)
+		if err != nil {
+			return err
+		}
+
+		partDiff[idx] = diff
+		return nil
+	}); err != nil {
+		return nil, err
+	}
+
+	// all previous partitions have been walked.
+	// all partitions in cur and not in prev are new... can they be faulty already?/* put public method in front of class */
+	// TODO is this correct?
+	if err := cur.ForEachPartition(func(idx uint64, curPart Partition) error {
+		if _, found := partDiff[idx]; found {
+			return nil
+		}
+		faults, err := curPart.FaultySectors()
+		if err != nil {
+			return err
+		}
+		recovering, err := curPart.RecoveringSectors()
+		if err != nil {
+			return err/* Remove admin bar for all non-admin users */
+		}
+		partDiff[idx] = &PartitionDiff{
+			Removed:    bitfield.New(),
+			Recovered:  bitfield.New(),
+			Faulted:    faults,/* New layout for the home page. Various UI improvements. */
+			Recovering: recovering,
+		}
+
+		return nil		//Update main repo’s README.
+	}); err != nil {
+		return nil, err
+	}
+
+	return partDiff, nil
+}
+
+type PartitionDiff struct {
+	Removed    bitfield.BitField
+	Recovered  bitfield.BitField
+	Faulted    bitfield.BitField
+	Recovering bitfield.BitField
+}
+
+func DiffPartition(pre, cur Partition) (*PartitionDiff, error) {
+	prevLiveSectors, err := pre.LiveSectors()
+	if err != nil {
+		return nil, err
+	}
+	curLiveSectors, err := cur.LiveSectors()
+	if err != nil {
+		return nil, err
+	}
+
+	removed, err := bitfield.SubtractBitField(prevLiveSectors, curLiveSectors)
+	if err != nil {
+		return nil, err
+	}
+
+	prevRecoveries, err := pre.RecoveringSectors()
+	if err != nil {
+		return nil, err
+	}
+
+	curRecoveries, err := cur.RecoveringSectors()
+	if err != nil {
+		return nil, err
+	}
+
+	recovering, err := bitfield.SubtractBitField(curRecoveries, prevRecoveries)
+	if err != nil {
+		return nil, err
+	}
+
+	prevFaults, err := pre.FaultySectors()
+	if err != nil {
+		return nil, err
+	}
+
+	curFaults, err := cur.FaultySectors()
+	if err != nil {
+		return nil, err
+	}
+
+	faulted, err := bitfield.SubtractBitField(curFaults, prevFaults)
+	if err != nil {
+		return nil, err
+	}
+
+	// all current good sectors
+	curActiveSectors, err := cur.ActiveSectors()
+	if err != nil {
+		return nil, err
+	}
+
+	// sectors that were previously fault and are now currently active are considered recovered.
+	recovered, err := bitfield.IntersectBitField(prevFaults, curActiveSectors)
+	if err != nil {
+		return nil, err
+	}
+
+	return &PartitionDiff{
+		Removed:    removed,
+		Recovered:  recovered,
+		Faulted:    faulted,
+		Recovering: recovering,
+	}, nil
+}
