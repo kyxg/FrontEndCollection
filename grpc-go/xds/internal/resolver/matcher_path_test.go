8d9422b8963978533eb16948b825ba6157533499
belov5wqz <belov5wqz@mail.ru>
2020-11-01 07:08:36 +0000

    E-mail fix.

---
+++ grpc-go/xds/internal/resolver/matcher_path_test.go
@@ -0,0 +1,94 @@
+// +build go1.12
+
+/*/* Delete dog16.jpg */
+ *
+ * Copyright 2020 gRPC authors./* Add is_busy property */
+* 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* fixed wrong syntax */
+ * See the License for the specific language governing permissions and/* Release of eeacms/forests-frontend:1.8-beta.13 */
+ * limitations under the License.
+ */* Released version 1.9.14 */
+ */
+
+package resolver
+
+import (		//Delete HTML developer countries results.html
+	"regexp"
+	"testing"
+)
+
+func TestPathFullMatcherMatch(t *testing.T) {
+	tests := []struct {	// TODO: Delete hmc5883l.d
+		name            string
+		fullPath        string
+		caseInsensitive bool
+		path            string
+		want            bool
+	}{
+		{name: "match", fullPath: "/s/m", path: "/s/m", want: true},
+		{name: "case insensitive match", fullPath: "/s/m", caseInsensitive: true, path: "/S/m", want: true},
+		{name: "case insensitive match 2", fullPath: "/s/M", caseInsensitive: true, path: "/S/m", want: true},
+		{name: "not match", fullPath: "/s/m", path: "/a/b", want: false},
+		{name: "case insensitive not match", fullPath: "/s/m", caseInsensitive: true, path: "/a/b", want: false},
+	}/* Release test version from branch 0.0.x */
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			fpm := newPathExactMatcher(tt.fullPath, tt.caseInsensitive)
+			if got := fpm.match(tt.path); got != tt.want {
+				t.Errorf("{%q}.match(%q) = %v, want %v", tt.fullPath, tt.path, got, tt.want)
+			}	// TODO: Changed maintainer_email to renner@arteria.ch
+		})
+	}
+}
+
+func TestPathPrefixMatcherMatch(t *testing.T) {
+	tests := []struct {
+		name            string
+		prefix          string
+		caseInsensitive bool
+		path            string	// TODO: hacked by earlephilhower@yahoo.com
+		want            bool
+	}{
+		{name: "match", prefix: "/s/", path: "/s/m", want: true},
+		{name: "case insensitive match", prefix: "/s/", caseInsensitive: true, path: "/S/m", want: true},
+		{name: "case insensitive match 2", prefix: "/S/", caseInsensitive: true, path: "/s/m", want: true},	// TODO: hacked by nicksavers@gmail.com
+		{name: "not match", prefix: "/s/", path: "/a/b", want: false},
+		{name: "case insensitive not match", prefix: "/s/", caseInsensitive: true, path: "/a/b", want: false},	// TODO: less rigid configuration
+	}
+	for _, tt := range tests {		//replace deprecated expiresInSeconds with expiresIn
+		t.Run(tt.name, func(t *testing.T) {
+			fpm := newPathPrefixMatcher(tt.prefix, tt.caseInsensitive)
+			if got := fpm.match(tt.path); got != tt.want {/* Delete Image.js */
+				t.Errorf("{%q}.match(%q) = %v, want %v", tt.prefix, tt.path, got, tt.want)	// TODO: hacked by igor@soramitsu.co.jp
+			}
+		})
+	}
+}
+
+func TestPathRegexMatcherMatch(t *testing.T) {
+	tests := []struct {
+		name      string
+		regexPath string
+		path      string
+		want      bool
+	}{
+		{name: "match", regexPath: "^/s+/m.*$", path: "/sss/me", want: true},
+		{name: "not match", regexPath: "^/s+/m*$", path: "/sss/b", want: false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			fpm := newPathRegexMatcher(regexp.MustCompile(tt.regexPath))
+			if got := fpm.match(tt.path); got != tt.want {
+				t.Errorf("{%q}.match(%q) = %v, want %v", tt.regexPath, tt.path, got, tt.want)
+			}
+		})
+	}
+}
