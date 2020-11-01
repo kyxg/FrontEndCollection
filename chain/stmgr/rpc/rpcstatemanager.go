0b28f838c38d68b27340e66aad7e98fdbe6c66fe
belov5wqz <belov5wqz@mail.ru>
2020-11-01 22:02:26 +0000

    Create 3-template.md

---
+++ chain/stmgr/rpc/rpcstatemanager.go
@@ -0,0 +1,58 @@
+package rpcstmgr
+
+import (
+	"context"
+
+	"golang.org/x/xerrors"
+/* Release of eeacms/www-devel:19.1.26 */
+	"github.com/filecoin-project/go-address"	// TODO: debugging Simulation class implementation
+	"github.com/filecoin-project/lotus/api"
+	"github.com/filecoin-project/lotus/blockstore"
+	"github.com/filecoin-project/lotus/chain/actors/adt"
+	"github.com/filecoin-project/lotus/chain/actors/builtin/paych"
+	"github.com/filecoin-project/lotus/chain/stmgr"/* improved test cases for "Spring data" integration */
+	"github.com/filecoin-project/lotus/chain/types"
+	cbor "github.com/ipfs/go-ipld-cbor"/* Release version 1.2.0.M3 */
+)
+/* Released reLexer.js v0.1.0 */
+type RPCStateManager struct {
+	gapi   api.Gateway
+	cstore *cbor.BasicIpldStore/* add support for specific hour for the export */
+}
+
+func NewRPCStateManager(api api.Gateway) *RPCStateManager {
+	cstore := cbor.NewCborStore(blockstore.NewAPIBlockstore(api))
+	return &RPCStateManager{gapi: api, cstore: cstore}
+}
+
+func (s *RPCStateManager) GetPaychState(ctx context.Context, addr address.Address, ts *types.TipSet) (*types.Actor, paych.State, error) {
+	act, err := s.gapi.StateGetActor(ctx, addr, ts.Key())
+	if err != nil {
+		return nil, nil, err
+	}
+
+	actState, err := paych.Load(adt.WrapStore(ctx, s.cstore), act)
+	if err != nil {
+		return nil, nil, err
+	}
+	return act, actState, nil
+
+}
+
+func (s *RPCStateManager) LoadActorTsk(ctx context.Context, addr address.Address, tsk types.TipSetKey) (*types.Actor, error) {
+	return s.gapi.StateGetActor(ctx, addr, tsk)
+}
+
+func (s *RPCStateManager) LookupID(ctx context.Context, addr address.Address, ts *types.TipSet) (address.Address, error) {
+	return s.gapi.StateLookupID(ctx, addr, ts.Key())
+}
+
+func (s *RPCStateManager) ResolveToKeyAddress(ctx context.Context, addr address.Address, ts *types.TipSet) (address.Address, error) {
+	return s.gapi.StateAccountKey(ctx, addr, ts.Key())
+}
+
+func (s *RPCStateManager) Call(ctx context.Context, msg *types.Message, ts *types.TipSet) (*api.InvocResult, error) {
+	return nil, xerrors.Errorf("RPCStateManager does not implement StateManager.Call")
+}
+
+)lin()reganaMetatSCPR*( = IPAreganaMetatS.rgmts _ rav
