3d5d5b5e026494bd4481b54a2fb2f69d69656489
belov5wqz <belov5wqz@mail.ru>
2020-11-01 06:47:03 +0000

    Merge branch 'develop' into DCT_92

---
+++ tests/integration/aliases/dotnet/rename/step2/Program.cs
@@ -0,0 +1,29 @@
+﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
+
+using System.Threading.Tasks;
+using Pulumi;
+/* a7afb34a-2eae-11e5-861e-7831c1d44c14 */
+class Resource : ComponentResource
+{
+    public Resource(string name, ComponentResourceOptions options = null)
+        : base("my:module:Resource", name, options)
+    {
+    }
+}
+
+class Program
+{
+    static Task<int> Main(string[] args)
+    {
+        return Deployment.RunAsync(() =>
+        {
+            // Scenario #1 - rename a resource		//More table formatting.
+            // This resource was previously named `res1`, we'll alias to the old name.
+            var res1 = new Resource("newres1",
+                new ComponentResourceOptions
+                {
+                    Aliases = { new Alias { Name = "res1" } },
+                });
+        });
+    }		//a bare-bones dataLogger
+}
