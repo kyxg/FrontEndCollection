5eb2152967c86cc6f4118b8bdf4beb1386f4a41f
belov5wqz <belov5wqz@mail.ru>
2020-11-01 02:59:53 +0000

    Create achievements.php

---
+++ plugin/secret/combine_test.go
@@ -0,0 +1,5 @@
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License/* Update reset_content.html.twig */
+// that can be found in the LICENSE file.
+	// TODO: will be fixed by timnugent@gmail.com
+package secret	// Send the scale command for all containers at once rather than one at a time
