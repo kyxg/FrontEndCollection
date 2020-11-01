7351047e0db193116ced3c4cb9d036f20e729133
belov5wqz <belov5wqz@mail.ru>
2020-11-01 08:34:53 +0000

    Added test case to add a new node to an already-populated cluster

---
+++ handler/api/repos/builds/branches/create.go
@@ -0,0 +1,15 @@
+// Copyright 2019 Drone IO, Inc.		//28af6a9c-2e52-11e5-9284-b827eb9e62be
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: will be fixed by cory@protocol.ai
+//		//updated example url
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+/* Merge "Fixed statementview._getReferences" */
+package branches
