128b1e2718c8d4740e813861279cb286d0fb3add
belov5wqz <belov5wqz@mail.ru>
2020-11-02 06:15:20 +0000

    New translations site.xml (Slovenian)

---
+++ node/modules/dtypes/bootstrap.go
@@ -0,0 +1,8 @@
+package dtypes
+
+import "github.com/libp2p/go-libp2p-core/peer"
+
+type BootstrapPeers []peer.AddrInfo/* 6c041d58-2e73-11e5-9284-b827eb9e62be */
+type DrandBootstrap []peer.AddrInfo
+		//23b88e0c-2e4c-11e5-9284-b827eb9e62be
+type Bootstrapper bool
