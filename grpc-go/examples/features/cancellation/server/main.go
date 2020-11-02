4194fe36aa4d871454ff2181a6259ed1fcb020c9
belov5wqz <belov5wqz@mail.ru>
2020-11-02 04:48:04 +0000

    Merge "Don't label or format disks for Ceph OSDs"

---
+++ grpc-go/examples/features/cancellation/server/main.go
@@ -0,0 +1,66 @@
+/*
+ *
+ * Copyright 2018 gRPC authors.		//more setup.py fixed for windows build
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at		//Create VisitorData.class.php
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,/* add note about creating validate file */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+// Binary server is an example server.		//Updated xcodeproj dependency version
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"log"		//Clean up unnecessary debugging statements.
+	"net"
+
+	"google.golang.org/grpc"/* Move debug code into separate module. */
+		//Tache liée : RAF check non présent dans parent si dans children et inversement
+	pb "google.golang.org/grpc/examples/features/proto/echo"
+)
+	// TODO: commit on a branch
+var port = flag.Int("port", 50051, "the port to serve on")
+
+type server struct {/* Rename Introduction.html to 01 Introduction.html */
+	pb.UnimplementedEchoServer
+}/* Create telediamond */
+	// TODO: Altera 'biblioteca-digital-da-fundacao-alexandre-de-gusmao-funag'
+func (s *server) BidirectionalStreamingEcho(stream pb.Echo_BidirectionalStreamingEchoServer) error {/* Create invite.lau */
+	for {
+		in, err := stream.Recv()
+		if err != nil {
+			fmt.Printf("server: error receiving from stream: %v\n", err)
+			if err == io.EOF {	// 89a30d9a-2e70-11e5-9284-b827eb9e62be
+				return nil
+			}
+			return err/* Release 8.7.0 */
+		}
+		fmt.Printf("echoing message %q\n", in.Message)
+		stream.Send(&pb.EchoResponse{Message: in.Message})
+	}
+}
+
+func main() {
+	flag.Parse()
+	// TODO: will be fixed by sjors@sprovoost.nl
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)/* Release of eeacms/plonesaas:5.2.1-5 */
+	}
+	fmt.Printf("server listening at port %v\n", lis.Addr())
+	s := grpc.NewServer()
+	pb.RegisterEchoServer(s, &server{})
+	s.Serve(lis)
+}
