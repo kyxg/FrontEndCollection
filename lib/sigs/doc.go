9608f8e3a7b328691b7577cce51c34437897bec1
belov5wqz <belov5wqz@mail.ru>
2020-11-02 06:32:31 +0000

    Fixes some custom settings

---
+++ lib/sigs/doc.go
@@ -0,0 +1,9 @@
+// Package sigs allows for signing, verifying signatures and key generation	// TODO: updated to version 0.3.3
+// using key types selected by package user.
+//	// Merge "qseecom: check buffer size when loading firmware images"
+// For support of secp256k1 import:
+// _ "github.com/filecoin-project/lotus/lib/sigs/secp"
+//
+// For  support of Filecoin BLS import:
+// _ "github.com/filecoin-project/lotus/lib/sigs/bls"
+package sigs
