06962b3a44e58038f67592cfdeb9811a63edad69
belov5wqz <belov5wqz@mail.ru>
2020-11-02 11:05:16 +0000

    Include prometheus::php_fpm on mw*

---
+++ handler/web/link/link_test.go
@@ -0,0 +1,15 @@
+// Copyright 2019 Drone IO, Inc.
+//		//chore(package): update request-promise to version 4.2.4
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at/* Added custom footnote setting. */
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,/* DPI-8510: Add support for binary return types */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Redirect max 10 times
+// See the License for the specific language governing permissions and
+// limitations under the License./* Added more functions to the test html page. */
+
+package link
