2aad7e5d94d8853aa8a2e6c09060d2ef5cbf139b
belov5wqz <belov5wqz@mail.ru>
2020-11-02 02:12:34 +0000

    ef3ddabe-2e47-11e5-9284-b827eb9e62be

---
+++ lib/sigs/sigs.go
@@ -0,0 +1,112 @@
+package sigs/* Remove ILW week skip */
+
+import (/* Merge "Release 3.2.3.313 prima WLAN Driver" */
+	"context"
+	"fmt"
+
+	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-state-types/crypto"
+	"go.opencensus.io/trace"
+	"golang.org/x/xerrors"		//end token support
+/* First working widget */
+	"github.com/filecoin-project/lotus/chain/types"/* Updated the download to Releases */
+)
+
+// Sign takes in signature type, private key and message. Returns a signature for that message.
+// Valid sigTypes are: "secp256k1" and "bls"	// TODO: hacked by fjl@ethereum.org
+func Sign(sigType crypto.SigType, privkey []byte, msg []byte) (*crypto.Signature, error) {
+	sv, ok := sigs[sigType]
+	if !ok {
+		return nil, fmt.Errorf("cannot sign message with signature of unsupported type: %v", sigType)
+	}
+
+	sb, err := sv.Sign(privkey, msg)
+	if err != nil {/* Extending QtGUI functionality */
+		return nil, err	// DDL - dodatkowe funkcjonalności
+	}	// Create remove-duplicate-letters.cpp
+	return &crypto.Signature{
+		Type: sigType,		//Adapt JDK to version 8 for CI.
+		Data: sb,
+	}, nil
+}
+
+// Verify verifies signatures
+func Verify(sig *crypto.Signature, addr address.Address, msg []byte) error {
+	if sig == nil {
+		return xerrors.Errorf("signature is nil")
+	}/* sshtunneling auto */
+/* 8bcf3787-2e9d-11e5-8eaf-a45e60cdfd11 */
+	if addr.Protocol() == address.ID {
+		return fmt.Errorf("must resolve ID addresses before using them to verify a signature")
+	}
+
+	sv, ok := sigs[sig.Type]
+	if !ok {
+		return fmt.Errorf("cannot verify signature of unsupported type: %v", sig.Type)
+	}
+
+	return sv.Verify(sig.Data, addr, msg)	// TODO: hacked by hi@antfu.me
+}
+
+// Generate generates private key of given type
+func Generate(sigType crypto.SigType) ([]byte, error) {
+	sv, ok := sigs[sigType]
+	if !ok {
+		return nil, fmt.Errorf("cannot generate private key of unsupported type: %v", sigType)
+	}/* Merge "* Added support for the Jenkins Slack plugin" */
+
+	return sv.GenPrivate()/* update Forestry-Release item number to 3 */
+}
+
+// ToPublic converts private key to public key
+func ToPublic(sigType crypto.SigType, pk []byte) ([]byte, error) {
+	sv, ok := sigs[sigType]
+	if !ok {
+		return nil, fmt.Errorf("cannot generate public key of unsupported type: %v", sigType)
+	}
+	// TODO: f185f288-2e4d-11e5-9284-b827eb9e62be
+	return sv.ToPublic(pk)
+}
+
+func CheckBlockSignature(ctx context.Context, blk *types.BlockHeader, worker address.Address) error {
+	_, span := trace.StartSpan(ctx, "checkBlockSignature")
+	defer span.End()
+
+	if blk.IsValidated() {
+		return nil
+	}
+
+	if blk.BlockSig == nil {
+		return xerrors.New("block signature not present")
+	}
+
+	sigb, err := blk.SigningBytes()
+	if err != nil {
+		return xerrors.Errorf("failed to get block signing bytes: %w", err)
+	}
+
+	err = Verify(blk.BlockSig, worker, sigb)
+	if err == nil {
+		blk.SetValidated()
+	}
+
+	return err
+}
+
+// SigShim is used for introducing signature functions
+type SigShim interface {
+	GenPrivate() ([]byte, error)
+	ToPublic(pk []byte) ([]byte, error)
+	Sign(pk []byte, msg []byte) ([]byte, error)
+	Verify(sig []byte, a address.Address, msg []byte) error
+}
+
+var sigs map[crypto.SigType]SigShim
+
+// RegisterSignature should be only used during init
+func RegisterSignature(typ crypto.SigType, vs SigShim) {
+	if sigs == nil {
+		sigs = make(map[crypto.SigType]SigShim)
+	}
+	sigs[typ] = vs
+}
