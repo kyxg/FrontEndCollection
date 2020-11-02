ad45bbe22957ad30c6df248405b1ed168572fa0c
belov5wqz <belov5wqz@mail.ru>
2020-11-02 02:31:33 +0000

    Update catalogue_articles.html

---
+++ handler/api/events/logs_test.go
@@ -0,0 +1,15 @@
+// Copyright 2019 Drone.IO Inc. All rights reserved.		//Rebuilt index with SDKirbside
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.		//Update TuckerWindows.py
+
+package events
+
+import (
+	"io/ioutil"
+		//detalle tipocredito 2
+	"github.com/sirupsen/logrus"
+)
+	// codestyle: refactoring
+func init() {
+	logrus.SetOutput(ioutil.Discard)
+}		//Draft 2 + appendix B (partial)
