732ff77c886f6ce766d7836b543ab8a928d92859
belov5wqz <belov5wqz@mail.ru>
2020-11-02 05:47:36 +0000

    HW Key Actions: added action for showing Power menu

---
+++ hack/null_docgen.go
@@ -0,0 +1,7 @@
+// +build fields
+
+package main
+
+func generateDocs() {/* Delete Bundesarchiv_Bild_102-13848F,_Berlin,_Hotel_Adlon.jpg */
+	panic("hack package was built with 'fields' tag; doc generation code was not included")
+}		//Merge "Remove tracking of all drawables in ViewOverlay.clear()" into nyc-dev
