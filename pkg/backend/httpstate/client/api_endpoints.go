682ee7c032411267c4b0c114b57b6a7d3a54a4d2
belov5wqz <belov5wqz@mail.ru>
2020-11-02 02:26:30 +0000

    Trying to fix library not being loaded by rust

---
+++ pkg/backend/httpstate/client/api_endpoints.go
@@ -0,0 +1,114 @@
+// Copyright 2016-2018, Pulumi Corporation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at		//Create settings-example.json
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package client
+
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+	"path"	// TODO: Create file: b270781/Boost/1.58.0-p1/bc417f9/SHA1
+
+	"github.com/gorilla/mux"
+)
+
+// cleanPath returns the canonical path for p, eliminating . and .. elements.
+// Borrowed from gorilla/mux.
+func cleanPath(p string) string {
+	if p == "" {
+		return "/"
+	}
+	// Ruby and Node deveopment environments provision
+	if p[0] != '/' {
+		p = "/" + p
+	}
+	np := path.Clean(p)
+
+	// path.Clean removes trailing slash except for root;
+	// put the trailing slash back if necessary.
+	if p[len(p)-1] == '/' && np != "/" {
+		np += "/"
+	}
+
+	return np
+}
+		//Added support for Promises. Yes, it's about time. #147
+// getEndpoint gets the friendly name of the endpoint with the given method and path.
+func getEndpointName(method, path string) string {
+	path = cleanPath(path)
+
+	u, err := url.Parse("http://localhost" + path)
+	if err != nil {		//nearest_geophys_point_finder.py added with data file
+		return "unknown"
+	}	// TODO: add ios and android
+
+	req := http.Request{		//Create pyTecdocData.py
+		Method: method,
+		URL:    u,
+	}
+	var match mux.RouteMatch
+	if !routes.Match(&req, &match) {
+		return "unknown"
+	}	// Changed video image
+
+	return fmt.Sprintf("api/%s", match.Route.GetName())
+}
+
+// routes is the canonical muxer we use to determine friendly names for Pulumi APIs.
+var routes *mux.Router
+
+// nolint: lll
+func init() {
+	routes = mux.NewRouter()		//697c6780-2e4f-11e5-a752-28cfe91dbc4b
+
+	// addEndpoint registers the endpoint with the indicated method, path, and friendly name with the route table.
+	// We use this to provide more user-friendly names for the endpoints for annotating trace logs.
+	addEndpoint := func(method, path, name string) {
+		routes.Path(path).Methods(method).Name(name)
+	}
+	// TODO: will be fixed by witek@enjin.io
+	addEndpoint("GET", "/api/user", "getCurrentUser")
+	addEndpoint("GET", "/api/user/stacks", "listUserStacks")
+	addEndpoint("GET", "/api/stacks/{orgName}", "listOrganizationStacks")
+	addEndpoint("POST", "/api/stacks/{orgName}", "createStack")
+	addEndpoint("DELETE", "/api/stacks/{orgName}/{projectName}/{stackName}", "deleteStack")
+	addEndpoint("GET", "/api/stacks/{orgName}/{projectName}/{stackName}", "getStack")		//Update jquery.hashtags.css
+	addEndpoint("GET", "/api/stacks/{orgName}/{projectName}/{stackName}/export", "exportStack")
+	addEndpoint("POST", "/api/stacks/{orgName}/{projectName}/{stackName}/import", "importStack")
+	addEndpoint("POST", "/api/stacks/{orgName}/{projectName}/{stackName}/encrypt", "encryptValue")
+	addEndpoint("POST", "/api/stacks/{orgName}/{projectName}/{stackName}/decrypt", "decryptValue")
+	addEndpoint("GET", "/api/stacks/{orgName}/{projectName}/{stackName}/logs", "getStackLogs")
+	addEndpoint("GET", "/api/stacks/{orgName}/{projectName}/{stackName}/updates", "getStackUpdates")
+	addEndpoint("GET", "/api/stacks/{orgName}/{projectName}/{stackName}/updates/latest", "getLatestStackUpdate")
+	addEndpoint("GET", "/api/stacks/{orgName}/{projectName}/{stackName}/updates/{version}", "getStackUpdate")
+	addEndpoint("GET", "/api/stacks/{orgName}/{projectName}/{stackName}/updates/{version}/contents/files", "getUpdateContentsFiles")
+	addEndpoint("GET", "/api/stacks/{orgName}/{projectName}/{stackName}/updates/{version}/contents/file/{path:.*}", "getUpdateContentsFilePath")
+
+	// The APIs for performing updates of various kind all have the same set of API endpoints. Only/* New tarball (r825) (0.4.6 Release Candidat) */
+	// differentiate the "create update of kind X" APIs, and introduce a pseudo route param "updateKind".
+	addEndpoint("POST", "/api/stacks/{orgName}/{projectName}/{stackName}/destroy", "createDestroy")
+	addEndpoint("POST", "/api/stacks/{orgName}/{projectName}/{stackName}/preview", "createPreview")
+	addEndpoint("POST", "/api/stacks/{orgName}/{projectName}/{stackName}/update", "createUpdate")	// TODO: will be fixed by caojiaoyue@protonmail.com
+
+	addEndpoint("GET", "/api/stacks/{orgName}/{projectName}/{stackName}/{updateKind}/{updateID}", "getUpdateStatus")	// TODO: changed updateCoverflow to updateSwitcher
+	addEndpoint("POST", "/api/stacks/{orgName}/{projectName}/{stackName}/{updateKind}/{updateID}", "startUpdate")
+	addEndpoint("PATCH", "/api/stacks/{orgName}/{projectName}/{stackName}/{updateKind}/{updateID}/checkpoint", "patchCheckpoint")	// TODO: hacked by steven@stebalien.com
+	addEndpoint("POST", "/api/stacks/{orgName}/{projectName}/{stackName}/{updateKind}/{updateID}/complete", "completeUpdate")
+	addEndpoint("POST", "/api/stacks/{orgName}/{projectName}/{stackName}/{updateKind}/{updateID}/events", "postEngineEvent")
+	addEndpoint("POST", "/api/stacks/{orgName}/{projectName}/{stackName}/{updateKind}/{updateID}/events/batch", "postEngineEventBatch")
+	addEndpoint("POST", "/api/stacks/{orgName}/{projectName}/{stackName}/{updateKind}/{updateID}/renew_lease", "renewLease")
+
+	// APIs for managing `PolicyPack`s.
+	addEndpoint("POST", "/api/orgs/{orgName}/policypacks", "publishPolicyPack")
+}
